4f5c8dbbe5831b43bbba6fea8660d1d2975accc4
Chibikem <chibikemjames@gmail.com>
2026-08-11 10:39:51 +0100

    Remove debug line

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from dotenv import load_dotenv
 from memory import save_fact, build_system_prompt, extract_memory, save_session, get_all_sessions, load_session
 
 load_dotenv()
-url = os.getenv('REDIS_URL', 'MISSING')
-st.write(f"DEBUG - Length: {len(url)} | First 30: {url[:30]} | Chars 40-70: {url[40:70]}")
+
 
 client = OpenAI(
     api_key=os.getenv("GROQ_API_KEY"),
